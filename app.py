2ccc40aea1d306681d5d79244c5e1e29c565bb07
alysafoong <alysafoongmenhua@gmail.com>
2025-07-22 16:38:01 +0800

    Update UI with explanatory markdown again

--- app.py
+++ app.py
@@ -64,80 +64,106 @@ st.title("Cardiovascular Risk Calculator")
 # Input form
 with st.form("input_form"):
     Sex = st.selectbox("Gender", list(sex_map.keys()))
-    
+
+    st.markdown("**General Health**")
     st.markdown("_Would you say that your health in general is: Poor (0), Fair (1), Good (2), Very Good (3), or Excellent (4)?_")
-    GeneralHealth = st.slider("General Health", 0, 4, 2)
+    GeneralHealth = st.slider("", 0, 4, 2)
 
+    st.markdown("**Physical Health**")
     st.markdown("_Now thinking about your physical health, which includes physical illness and injury, for how many days during the past 30 days was your physical health **not good**?_")
-    PhysicalHealthDays = st.slider("Physical Health", 0, 30, 0)
+    PhysicalHealthDays = st.slider("", 0, 30, 0)
 
+    st.markdown("**Mental Health**")
     st.markdown("_Now thinking about your mental health, which includes stress, depression, and problems with emotions, for how many days during the past 30 days was your mental health **not good**?_")
-    MentalHealthDays = st.slider("Mental Health", 0, 30, 0)
-    
+    MentalHealthDays = st.slider("", 0, 30, 0)
+
+    st.markdown("**Last Medical Checkup Time**")
     st.markdown("_About how long has it been since you last visited a doctor for a routine checkup?_")
-    LastCheckupTime = st.selectbox("Last Medical Checkup Time", list(last_checkup_map.keys()))
-    
+    LastCheckupTime = st.selectbox("", list(last_checkup_map.keys()))
+
+    st.markdown("**Physical Activities**")
     st.markdown("_During the **past month, other than your regular job**, did you participate in any physical activities or exercises such as running, calisthenics, golf, gardening, or walking for exercise?_")
-    PhysicalActivities = st.selectbox("Physical Activities", list(yes_no_map.keys()))
-    
+    PhysicalActivities = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Sleep Hours**")
     st.markdown("_On average, how many hours of sleep do you get in a 24-hour period?_")
-    SleepHours = st.slider("Sleep Hours", 0, 24, 7)
-    
+    SleepHours = st.slider("", 0, 24, 7)
+
+    st.markdown("**Number of Permanent Teeth Removed**")
     st.markdown("_Not including teeth lost for injury or orthodontics, how many of your permanent teeth have been removed because of tooth decay or gum disease?_")
-    RemovedTeeth = st.selectbox("Number of Permanent Teeth Removed", list(removed_teeth_map.keys()))
-    
+    RemovedTeeth = st.selectbox("", list(removed_teeth_map.keys()))
+
+    st.markdown("**Angina or Coronary Heart Disease**")
     st.markdown("_Ever told you had angina or coronary heart disease?_")
-    HadAngina = st.selectbox("Angina or Coronary Heart Disease", list(yes_no_map.keys()))
-    
+    HadAngina = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Stroke**")
     st.markdown("_Ever told you had a stroke?_")
-    HadStroke = st.selectbox("Stroke", list(yes_no_map.keys()))
-    
+    HadStroke = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Asthma**")
     st.markdown("_Ever told you had asthma?_")
-    HadAsthma = st.selectbox("Asthma", list(yes_no_map.keys()))
-    
+    HadAsthma = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Skin Cancer**")
     st.markdown("_Ever told you had skin cancer that is not melanoma?_")
-    HadSkinCancer = st.selectbox("Skin Cancer", list(yes_no_map.keys()))
-    
+    HadSkinCancer = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**COPD**")
     st.markdown("_Ever told you had COPD (chronic obstructive pulmonary disease), emphysema or chronic bronchitis?_")
-    HadCOPD = st.selectbox("COPD", list(yes_no_map.keys()))
-    
+    HadCOPD = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Depressive Disorder**")
     st.markdown("_Ever told you had a depressive disorder (including depression, major depression, dysthymia, or minor depression)?_")
-    HadDepressiveDisorder = st.selectbox("Depressive Disorder", list(yes_no_map.keys()))
-    
+    HadDepressiveDisorder = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Kidney Disease**")
     st.markdown("_Not including kidney stones, bladder infection or incontinence, were you ever told you had kidney disease?_")
-    HadKidneyDisease = st.selectbox("Kidney Disease", list(yes_no_map.keys()))
-    
+    HadKidneyDisease = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Arthritis**")
     st.markdown("_Ever told you had some form of arthritis, rheumatiod arthritis, gout, lupus, or fibromyalgia?_")
-    HadArthritis = st.selectbox("Arthritis", list(yes_no_map.keys()))
-    
+    HadArthritis = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Diabetes**")
     st.markdown("_Ever told you had diabetes?_")
-    HadDiabetes = st.selectbox("Diabetes", list(diabetes_map.keys()))
-    
+    HadDiabetes = st.selectbox("", list(diabetes_map.keys()))
+
+    st.markdown("**Deaf or Hard of Hearing**")
     st.markdown("_Are you deaf or do you have serious difficulty hearing?_")
-    DeafOrHardOfHearing = st.selectbox("Deaf or Hard of Hearing", list(yes_no_map.keys()))
-    
+    DeafOrHardOfHearing = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Difficulty Concentrating or Remembering**")
     st.markdown("_Because of a physical, mental, or emotional condition, do you have serious difficulty concentrating, remembering, or making decisions?_")
-    DifficultyConcentrating = st.selectbox("Difficulty Concentrating or Remembering", list(yes_no_map.keys()))
-    
+    DifficultyConcentrating = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Difficulty Walking or Climbing Stairs**")
     st.markdown("_Do you have serious difficulty walking or climbing stairs?_")
-    DifficultyWalking = st.selectbox("Difficulty Walking or Climbing Stairs", list(yes_no_map.keys()))
-    
+    DifficultyWalking = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Difficulty Dressing or Bathing**")
     st.markdown("_Do you have difficulty dressing or bathing?_")
-    DifficultyDressingBathing = st.selectbox("Difficulty Dressing or Bathing", list(yes_no_map.keys()))
-    
-    SmokerStatus = st.selectbox("Smoking Status", list(smoker_map.keys()))
-    
+    DifficultyDressingBathing = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Smoking Status**")
+    SmokerStatus = st.selectbox("", list(smoker_map.keys()))
+
+    st.markdown("**Chest Scan**")
     st.markdown("_Have you ever had a CT or CAT scan of your chest area?_")
-    ChestScan = st.selectbox("Chest Scan", list(yes_no_map.keys()))
-    
-    RaceEthnicityCategory = st.selectbox("Race or Ethnicity", list(race_map.keys()))
-    
-    AgeCategory = st.selectbox("Age", list(age_map.keys()))
-    
-    BMI = st.slider("BMI", 10.0, 50.0, 22.0)
-    
+    ChestScan = st.selectbox("", list(yes_no_map.keys()))
+
+    st.markdown("**Race or Ethnicity**")
+    RaceEthnicityCategory = st.selectbox("", list(race_map.keys()))
+
+    st.markdown("**Age**")
+    AgeCategory = st.selectbox("", list(age_map.keys()))
+
+    st.markdown("**BMI**")
+    BMI = st.slider("", 10.0, 50.0, 22.0)
+
+    st.markdown("**Alcohol Drinking**")
     st.markdown("_Have you had at least one drink of alcohol in the **past 30 days**?_")
-    AlcoholDrinkers = st.selectbox("Alcohol Drinking", list(yes_no_map.keys()))
+    AlcoholDrinkers = st.selectbox("", list(yes_no_map.keys()))
 
     submitted = st.form_submit_button("Calculate Cardiovascular Risk")
 
@@ -181,6 +207,10 @@ if submitted:
 
     st.subheader("Cardiovascular Risk Calculator Results")
     st.write(f"Cardiovascular Risk: {prediction_proba:.2%}")
-    st.write("", "High Cardiovascular Risk" if prediction == 1 else "Low Cardiovascular Risk")
+    if prediction == 1:
+        st.error("High Cardiovascular Risk")
+    else:
+        st.success("Low Cardiovascular Risk")
+
     st.caption("_Note: A cardiovascular risk above 50% is considered high. Please consult a medical professional for further evaluation._")
 
